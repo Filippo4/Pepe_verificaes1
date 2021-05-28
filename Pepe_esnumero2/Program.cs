0de35c055ba9190d0fc813bc7e22b671fb7aca3f
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2021-05-28 09:10:01 +0200

    Add project files.

---
+++ Pepe_esnumero2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Pepe_esnumero2
+{
+    class Program
+    {
+        static SemaphoreSlim s1 = new SemaphoreSlim(1);
+        static SemaphoreSlim s2 = new SemaphoreSlim(1);
+
+        static int buffer = 0;
+        static Random r = new Random();
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(() => Metti());
+            Thread t2 = new Thread(() => Togli());
+            t1.Start();
+            t2.Start();
+            while (t1.IsAlive) { }
+            while (t2.IsAlive) { }
+        }
+        static void Metti()
+        {
+            int pari = r.Next(1, 100);
+            if(pari % 2 == 0)
+            {
+                if (buffer == 0)
+                {
+                    buffer = pari;
+                    Thread.Sleep(100);
+                }
+            }
+            s2.Release();
+            s1.Wait();
+        }
+        static void Togli()
+        {
+            if(buffer != 0)
+            {
+                Console.WriteLine($"usato numero {buffer}");
+                buffer = 0;
+                Thread.Sleep(50);
+
+            }
+            s1.Release();
+
+            s2.Wait();
+        }
+    }
+}
---
+++ Pepe_verificaes1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Pepe_verificaes1
+{
+    class Program
+    {
+        static string txt = "";
+        static int numB = 0;
+
+        
+        static SemaphoreSlim s1 = new SemaphoreSlim(1);
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(() => Metodo1());
+            t1.Start();
+            Thread t2 = new Thread(() => Metodo2());          
+            t2.Start();
+            while (t1.IsAlive) { }
+            while (t2.IsAlive) { }
+            Console.WriteLine($"{txt.Length} , {numB}");
+            Console.ReadLine();
+        }
+        
+        public static void Metodo1()
+        {
+            char a = 'a';
+           
+            for (int i = 0; i < 100; i++)
+            {
+                txt = a + txt;
+            }
+           
+        }
+
+        public static void Metodo2()
+        {
+            char b = 'b';
+            s1.Wait();
+            for (int i = 0; i < 100; i++)
+            {
+                if (txt != "")
+                {
+                    txt.Substring(0, txt.Length -1);
+                }
+                else if (txt != "" || txt[i] == b)
+                {
+                    txt = txt + b;
+                    numB++;
+                }
+            }
+            s1.Release();
+        }
+    }
+}
